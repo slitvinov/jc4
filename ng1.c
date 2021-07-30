5167c90627fd0fe984bfe1a18b2a479b8ceb04de
Sergey Litvinov <slitvinov@gmail.com>
2021-07-30 18:29:11 +0200

    add value iteration

--- ng1.c
+++ ng1.c
@@ -5,75 +5,99 @@ c99  ng1.c `pkg-config --libs --cflags gsl`
 #include <gsl/gsl_matrix_double.h>
 #include <gsl/gsl_linalg.h>
 
-enum {LEFT, STAY, RIGHT};
-enum
-{ NS = 4, NA = 3 };
+enum { LEFT, STAY, RIGHT };
+enum { NS = 4, NA = 3 };
 
 double P[NA][NS][NS] = {
-  {
-   {1, 0, 0, 0},
-   {1, 0, 0, 0},
-   {0, 1, 0, 0},
-   {0, 0, 1, 0},
-   },
-  {
-   {1, 0, 0, 0},
-   {0, 1, 0, 0},
-   {0, 0, 1, 0},
-   {0, 0, 0, 1},
-   },
-  {
-   {0, 1, 0, 0},
-   {0, 0, 1, 0},
-   {0, 0, 0, 1},
-   {0, 0, 0, 1},
-   },
+    {
+     { 1, 0, 0, 0},
+     { 1, 0, 0, 0},
+     { 0, 1, 0, 0},
+     { 0, 0, 1, 0},
+      },
+    {
+     { 1, 0, 0, 0},
+     { 0, 1, 0, 0},
+     { 0, 0, 1, 0},
+     { 0, 0, 0, 1},
+      },
+    {
+     { 0, 1, 0, 0},
+     { 0, 0, 1, 0},
+     { 0, 0, 0, 1},
+     { 0, 0, 0, 1},
+      },
 };
+
+int A[NA] = { LEFT, STAY, RIGHT };
+const char *Astr[NA] = { "<", "o", ">" };
 
 double R[NS] = {
-  1, 0, 0, 1,
+    1, 0, 0, 1,
 };
 
-double g = 4/5.0;
+double g = 4 / 5.0;
 
 int
 inv(double *a, double *b)
 {
-  int s;
-  gsl_matrix_view A;
-  gsl_matrix_view B;
+    int s;
+    gsl_matrix_view A;
+    gsl_matrix_view B;
 
-  A = gsl_matrix_view_array(a, NS, NS);
-  B = gsl_matrix_view_array(b, NS, NS);
-  
-  gsl_permutation *p = gsl_permutation_alloc(NS);
-  gsl_linalg_LU_decomp(&A.matrix, p, &s);
-  gsl_linalg_LU_invert(&A.matrix, p, &B.matrix);
-  
-  gsl_permutation_free(p);
+    A = gsl_matrix_view_array(a, NS, NS);
+    B = gsl_matrix_view_array(b, NS, NS);
+
+    gsl_permutation *p = gsl_permutation_alloc(NS);
+
+    gsl_linalg_LU_decomp(&A.matrix, p, &s);
+    gsl_linalg_LU_invert(&A.matrix, p, &B.matrix);
+
+    gsl_permutation_free(p);
 }
 
 int
-main ()
+main()
 {
-  int i, j;
-  double G[NS*NS];
-  double Ginv[NS*NS];
-  double V[NS];
-  int p[NS] = {RIGHT, STAY, STAY, LEFT};
+    int i, j, t;
+    double G[NS * NS];
+    double Ginv[NS * NS];
+    double V[NS];
+    int p[NS] = { RIGHT, STAY, STAY, LEFT };
 
-  for (i = 0; i < NS; i++)
-    for (j = 0; j < NS; j++)
-      G[NS * i + j] = (i == j) - g * P[p[i]][i][j];
-  inv(G, Ginv);
+    for (t = 0; t < 10; t++) {
 
-  for (i = 0; i < NS; i++) {
-    V[i] = 0;
-    for (j = 0; j < NS; j++)
-      V[i] += Ginv[NS * i + j] * R[j];
-  }
+        for (i = 0; i < NS; i++)
+            for (j = 0; j < NS; j++)
+                G[NS * i + j] = (i == j) - g * P[p[i]][i][j];
+        inv(G, Ginv);
 
-  for (i = 0; i < NS; i++)
-    printf("%6.2g ", V[i]);
-  printf("\n");
+        for (i = 0; i < NS; i++) {
+            V[i] = 0;
+            for (j = 0; j < NS; j++)
+                V[i] += Ginv[NS * i + j] * R[j];
+        }
+
+        double Q, Qmax, amax;
+        int a;
+
+        for (i = 0; i < NS; i++) {
+            Qmax = -DBL_MAX;
+            for (a = 0; a < NA; a++) {
+                Q = R[i];
+                for (j = 0; j < NS; j++) {
+                    Q += g * P[A[a]][i][j] * V[j];
+                }
+                if (Q > Qmax) {
+                    Qmax = Q;
+                    amax = A[a];
+                }
+            }
+            p[i] = amax;
+        }
+
+        for (i = 0; i < NS; i++)
+            printf("%6.2g:%s ", V[i], Astr[p[i]]);
+        printf("\n");
+    }
 }
