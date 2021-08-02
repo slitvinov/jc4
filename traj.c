639779acdaa4cc11a84db73eab6de45a85f53eca
Sergey Litvinov <slitvinov@gmail.com>
2021-08-02 17:22:11 +0200

    minor

--- traj.c
+++ traj.c
@@ -6,7 +6,7 @@
 #include <gsl/gsl_rng.h>
 
 enum { LEFT, STAY, RIGHT };
-enum { NS = 5, NA = 3, NT = 10, MAX_LEN = 100 };
+enum { NS = 5, NA = 3, NP = 2, NT = 100, MAX_LEN = 100 };
 const double P[NA][NS][NS] = {
     {
 	{0.0, 0.0, 0.0, 0.0, 1.0},
@@ -58,7 +58,7 @@ int rnd(gsl_rng *r, int lo, int hi)
   return lo + gsl_rng_uniform_int(r, hi - lo);
 }
 
-void gen_traj(void) {
+void traj_gen(void) {
   int t, s, s0, j, a;
   const gsl_rng_type * T;
   gsl_rng * r;
@@ -70,7 +70,6 @@ void gen_traj(void) {
    s = rnd(r, 0, NS - 1);
    for (j = 0; ; j++) {
      traj[t][j] = s;
-     fprintf(stderr, "s: %d:%d\n", t, s);
      if (s == NS - 1 || j == MAX_LEN - 1)
        break;
      do {
@@ -92,12 +91,32 @@ void traj_ret(const double *R) {
       ret += R[traj[t][j]];
     }
     Ret[t] = ret;
-    fprintf(stderr, "r: %d:%g\n", t, ret);
   }
 }
 
 void traj_prob(const int *p) {
-  
+  double prob;
+  int t, j, a, x, y;
+  for (t = 0; t < NT; t++) {
+    prob = 1;
+    for (j = 1; j < MAX_LEN && traj[t][j - 1] != NS - 1; j++) {
+      x = traj[t][j - 1];
+      y = traj[t][j];
+      a = p[x];
+      prob *= P[a][x][y];
+    }
+    Prob[t] = prob;
+  }
+}
+
+double traj_score(void)
+{
+  double ans;
+  int t;
+  ans = 0;
+  for (t = 0; t < NT; t++)
+    ans += Ret[t] * Prob[t];
+  return ans/NT;
 }
 
 void policy(const double *R, const double *V, int *p) {
@@ -127,18 +146,36 @@ void forward(const double *R, int *p) {
   }
 }
 
+void get_reward(const double *phi, double *R) {
+  int i;
+  for (i = 0; i < NS; i++)
+    R[i] = 0;
+  R[NS - 3] = phi[0];
+  R[NS - 2] = phi[1];
+}
+
 int main() {
   int i;
-  double V[NS];
-  int p[NS] = {STAY, STAY, STAY, STAY, STAY};
-  double R[NS] = {0, 0, 0, 1, 0};
-  
-  gen_traj();
-  traj_ret(R);
-  
-  forward(R, p);
-  value(R, p, V);
-  for (i = 0; i < NS - 1; i++)
-    printf("%6.2f%s ", V[i], Astr[p[i]]);
-  printf("\n");
+  double S, Sm, phi[NP], V[NS];
+  int p[NS] = {LEFT, LEFT, LEFT, LEFT, LEFT};
+  const double R[NS] = {0, 0, 0, 1, 0};
+  double Rth[NS];
+  traj_gen();
+  Sm = 0;
+  for (phi[0] = -2.0; phi[0] < 2.0; phi[0] += 0.1) {
+    for (phi[1] = -2.0; phi[1] < 2.0; phi[1] += 0.1) {
+      get_reward(phi, Rth);
+      forward(Rth, p);
+      value(Rth, p, V);
+      traj_prob(p);
+      traj_ret(R);
+      if ((S = traj_score()) > Sm) {
+	Sm = S;
+	fprintf(stderr, "score: %g %g       %g\n", phi[0], phi[1], Sm);
+	for (i = 0; i < NS - 1; i++)
+	  printf("%6.2f%s ", V[i], Astr[p[i]]);
+	printf("\n");
+      }
+    }
+  }
 }
